3383419f1924cb5c4d79df192cafa3ca12b1592f
Da-Youn <dayoun202@gmail.com>
2023-07-30 00:56:04 +0900

    🎨 Style : 타입 선언, 에러메시지/통과메시지 고려한 border-color 설정 #2

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,12 +1,21 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
-  max-width: 480px;
-  height: 60px;
-  border-bottom: 1px solid
-    ${(props: { $isError: boolean }) =>
-      props.$isError ? 'var(--error-color)' : 'var(--border-color)'};
-  margin-bottom: 10px;
+  max-width: ${(props: { maxWidth: string }) => props.maxWidth || '480px'};
+  width: ${(props: { width: string }) => props.width};
+  height: ${(props: { height: string }) => props.height || '54px'};
+  padding: ${(props: { padding: string }) => props.padding};
+  border-width: ${(props: { $borderWidth: string }) =>
+    props.$borderWidth || 'none'};
+  border-style: solid;
+  border-color: ${(props: { $isError: boolean; $isActive: boolean }) =>
+    props.$isError
+      ? 'var(--error-color)'
+      : props.$isActive
+      ? 'var(--main-color)'
+      : 'var(--border-color)'};
+  border-radius: ${(props: { $bdRadius: string }) => props.$bdRadius || 'none'};
+  margin-bottom: ${(props: { $mgBottom: string }) => props.$mgBottom || '10px'};
 
   &::placeholder {
     font-size: 16px;
