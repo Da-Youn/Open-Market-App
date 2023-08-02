caddbc39398a1bf0cb22b6c7057a928babbacb0b
Da-Youn <dayoun202@gmail.com>
2023-08-02 16:22:14 +0900

    🎨 Style : 스타일 컴포넌트 파일분리, 기능 태그 SignupForm으로 이동 #2

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,34 +1,11 @@
-import React, { useState, ChangeEvent, MouseEventHandler } from 'react';
-import styled from 'styled-components';
-import { Link, useNavigate } from 'react-router-dom';
-import Button from '../../components/common/Button';
-import { FormWrap } from '../../components/common/Form';
-import TypeChange from 'src/components/common/TypeChange';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import CoralLogo from '../../assets/logo-coral.png';
-import CheckOn from '../../assets/icon-check-on.svg';
-import CheckOff from '../../assets/icon-check-off.svg';
-import CheckBox from '../../assets/check-box.svg';
-import CheckBoxFilled from '../../assets/check-fill-box.svg';
-import DownArrow from '../../assets/icon-down-arrow.svg';
-import UpArrow from '../../assets/icon-up-arrow.svg';
 import SignupForm from 'src/components/common/Signup/SignupForm';
-import { Input } from 'src/components/common/Input';
+import { SignupWrap } from './SignupStyle';
 export interface SignupProps {}
 
 const Signup: React.FC<SignupProps> = () => {
-  const [checkBoxFiiled, setCheckBoxFiiled] = useState<string>(CheckBox);
-  const [checked, setChecked] = useState<boolean>(false);
-
-  const handleCheckBoxFilled = () => {
-    if (checked) {
-      setChecked(false);
-      setCheckBoxFiiled(CheckBox);
-    } else {
-      setChecked(true);
-      setCheckBoxFiiled(CheckBoxFilled);
-    }
-  };
-
   return (
     <SignupWrap>
       <header>
@@ -40,75 +17,9 @@ const Signup: React.FC<SignupProps> = () => {
       </header>
       <main>
         <SignupForm />
-        <CheckWrap>
-          <button onClick={handleCheckBoxFilled}>
-            <img src={checkBoxFiiled} alt='체크박스' />{' '}
-          </button>
-          <p>
-            호두샵의 <span>이용약관</span> 및 <span>개인정보처리방침</span>에
-            대한 내용을 확인하였고 동의합니다.
-          </p>
-        </CheckWrap>
-        <Button type='submit' $maxWidth='480px' $mgTop='34px' $bdRadius='5px'>
-          가입하기
-        </Button>
       </main>
     </SignupWrap>
   );
 };
-
-const SignupWrap = styled.div`
-  margin: 130px auto;
-  padding: 0 25px;
-  header,
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  main {
-    margin: auto;
-    max-width: 550px;
-  }
-
-  h1 {
-    margin-bottom: 70px;
-    img {
-      width: 268px;
-    }
-  }
-
-  label {
-    color: var(--sub-font-color);
-    margin-bottom: 10px;
-  }
-
-  .phone-number {
-    padding-left: 16px;
-    height: 54px;
-    border-radius: 5px;
-    border: 1px solid var(--border-color);
-  }
-`;
-
-const CheckWrap = styled.div`
-  display: flex;
-  gap: 10px;
-  color: var(--sub-font-color);
-
-  span {
-    font-weight: 700;
-    text-decoration-line: underline;
-  }
-`;
-
-const SignupButton = styled(Button)`
-  width: 100%;
-  max-width: 480px;
-  padding: 16px 0;
-  height: 60px;
-  margin-top: 34px;
-`;
 
 export default Signup;
---
+++ src/pages/Signup/SignupStyle.tsx
@@ -0,0 +1,196 @@
+import styled from 'styled-components';
+
+const SignupWrap = styled.div`
+  margin: 130px auto;
+  padding: 0 25px;
+  header,
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  main {
+    margin: auto;
+    max-width: 550px;
+  }
+
+  h1 {
+    margin-bottom: 70px;
+    img {
+      width: 268px;
+    }
+  }
+
+  label {
+    color: var(--sub-font-color);
+    margin-bottom: 10px;
+  }
+
+  .phone-number {
+    padding-left: 16px;
+    height: 54px;
+    border-radius: 5px;
+    border: 1px solid var(--border-color);
+  }
+`;
+
+const AccountInfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-bottom: 20px;
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+const IdInput = styled.div`
+  div {
+    gap: 12px;
+    flex-direction: row;
+    justify-content: flex-end;
+    flex-wrap: wrap;
+    input {
+      flex-grow: 1;
+    }
+  }
+`;
+const PasswordInput = styled.div`
+  position: relative;
+
+  &::after {
+    content: '';
+    width: 28px;
+    height: 28px;
+    background-image: url(${(props: { $pwValid: string }) => props.$pwValid});
+    position: absolute;
+    right: 10px;
+    top: 40px;
+  }
+`;
+const UserInfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+const NameInput = styled.div``;
+const PhoneNumberInput = styled.div`
+  div {
+    flex-direction: row;
+    gap: 12px;
+    input,
+    .phone-number {
+      width: 100%;
+    }
+  }
+
+  .phone-number {
+    position: relative;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding-right: 14px;
+
+    span {
+      flex: 9;
+    }
+    img {
+      flex: 1;
+      vertical-align: bottom;
+    }
+  }
+`;
+
+const FirstNumber = styled.div`
+  position: relative;
+  width: 100%;
+  ul {
+    text-align: center;
+    height: 150px;
+    position: absolute;
+    border-radius: 5px;
+    border: 1px solid var(--border-color);
+    background-color: var(--white);
+    overflow-y: scroll;
+    width: 100%;
+    left: 0;
+    top: 60px;
+
+    li {
+      button {
+        width: 100%;
+        padding: 5px 0;
+      }
+      button:hover {
+        background-color: var(--main-color);
+        color: var(--white);
+      }
+    }
+  }
+
+  ul::-webkit-scrollbar {
+    background-color: var(--sub-color);
+    width: 18px;
+  }
+  ul::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: var(--border-color);
+    background-clip: padding-box;
+    border: 5px solid transparent;
+  }
+`;
+
+const SellerInfoWrap = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const RegNumberInput = styled.div`
+  div {
+    gap: 12px;
+    flex-direction: row;
+    justify-content: flex-end;
+    flex-wrap: wrap;
+    input {
+      flex-grow: 1;
+    }
+  }
+`;
+
+const CheckWrap = styled.div`
+  display: flex;
+  gap: 10px;
+  color: var(--sub-font-color);
+
+  span {
+    font-weight: 700;
+    text-decoration-line: underline;
+  }
+`;
+
+export {
+  SignupWrap,
+  AccountInfoWrap,
+  IdInput,
+  PasswordInput,
+  UserInfoWrap,
+  NameInput,
+  PhoneNumberInput,
+  FirstNumber,
+  SellerInfoWrap,
+  RegNumberInput,
+  CheckWrap,
+};
