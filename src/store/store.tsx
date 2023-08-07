56fe7cbb061be3fa6f30fba9aed4a1c5ae8ff03f
Da-Youn <dayoun202@gmail.com>
2023-08-07 23:11:57 +0900

    🎨 Style : store.tsx 세팅 (productSlice도 미리 세팅) #8

---
+++ src/store/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productSlice from './productSlice';
+
+const store = configureStore({
+  reducer: {
+    product: productSlice,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
