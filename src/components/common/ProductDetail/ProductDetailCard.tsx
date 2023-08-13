789051aa43c1159277aa39c2dc8176ea09ad9aae
Da-Youn <dayoun202@gmail.com>
2023-08-13 15:17:03 +0900

    💄 Design : 상품 상세페이지 구현 #7

--- src/components/common/ProductDetail/ProductDetailCard.tsx
+++ src/components/common/ProductDetail/ProductDetailCard.tsx
@@ -3,6 +3,19 @@ import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductData } from 'src/store/productSlice';
 import { RootState } from 'src/store/store';
+import Button from '../Button';
+import PlusIcon from '../../../assets/icon-plus-line.svg';
+import MinusIcon from '../../../assets/icon-minus-line.svg';
+import {
+  ProductCardWrap,
+  InfoFormSecrion,
+  ProductInfo,
+  ProductName,
+  ProductForm,
+  ProductQuantitySelection,
+  ProductTotalAmount,
+  SubmitButtonWrap,
+} from './ProductDetailCardStyle';
 
 export interface ProductDetailCardProps {}
 
@@ -17,10 +30,79 @@ const ProductDetailCard: React.FC<ProductDetailCardProps> = () => {
 
   useEffect(() => {
     dispatch(getProductData(postId)); // createAsyncThunk로 비동기 요청 수행
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
+      {productData && (
+        <ProductCardWrap>
+          <h1 className='a11y-hidden'>상품 상세 정보</h1>
+          <div>
+            <img
+              aria-label='상품 이미지'
+              src={productData.image}
+              alt={`${productData.product_name} 이미지`}
+            />
+          </div>
+
+          <InfoFormSecrion>
+            <ProductInfo>
+              <h2 className='a11y-hidden'>상품 정보</h2>
+              <p aria-label='스토어 이름'>{productData.store_name}</p>
+              <ProductName aria-label='상품 이름'>
+                <strong>{productData.product_name}</strong>
+              </ProductName>
+              <p aria-label='상품 가격'>
+                <strong>{productData.price.toLocaleString()}</strong>원
+              </p>
+            </ProductInfo>
+
+            <ProductForm>
+              <h2 className='a11y-hidden'>구매하기</h2>
+              <p>택배배송 / 무료배송</p>
+              <ProductQuantitySelection>
+                <h3 className='a11y-hidden'>수량 선택</h3>
+                <div>
+                  <button>
+                    <img src={MinusIcon} alt='감소 버튼' />
+                  </button>
+                  <p>1</p>
+                  <button>
+                    <img src={PlusIcon} alt='증가 버튼' />
+                  </button>
+                </div>
+              </ProductQuantitySelection>
+              <ProductTotalAmount>
+                <h3>총 상품 금액</h3>
+                <div>
+                  <p>
+                    총 수량 <span>1</span>개
+                  </p>
+                  <p>
+                    <span>{productData.price.toLocaleString()}</span>원
+                  </p>
+                </div>
+              </ProductTotalAmount>
+              <SubmitButtonWrap>
+                <Button width='416px' fontWeight='400'>
+                  바로 구매
+                </Button>
+                <Button
+                  width='200px'
+                  fontWeight='400'
+                  bgColor='var(--sub-font-color )'
+                >
+                  장바구니
+                </Button>
+              </SubmitButtonWrap>
+            </ProductForm>
+          </InfoFormSecrion>
+        </ProductCardWrap>
+      )}
     </>
   );
 };
+
+export default ProductDetailCard;
---
+++ src/components/common/ProductDetail/ProductDetailCardStyle.tsx
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+
+export const ProductCardWrap = styled.section`
+  width: 100%;
+  display: flex;
+  gap: 50px;
+
+  img {
+    width: 600px;
+    height: 600px;
+    object-fit: cover;
+    object-position: 50% 0;
+  }
+`;
+
+export const InfoFormSecrion = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ProductInfo = styled.div`
+  font-size: var(--font-md);
+  strong {
+    font-size: var(--font-xl);
+  }
+
+  h2 + p {
+    color: var(--sub-font-color);
+  }
+
+  strong + p strong {
+    font-weight: 700;
+  }
+`;
+
+export const ProductName = styled.p`
+  margin: 16px 0 20px;
+`;
+
+export const ProductForm = styled.form`
+  h2 + p {
+    color: var(--sub-font-color);
+  }
+`;
+
+export const ProductQuantitySelection = styled.div`
+  padding: 30px 0;
+  margin: 20px 0 46px;
+  border-top: 1px solid var(--border-color);
+  border-bottom: 1px solid var(--border-color);
+
+  div {
+    width: 150px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    border: 1px solid var(--border-color);
+    font-size: var(--font-md);
+
+    button {
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 20px;
+        height: 20px;
+      }
+    }
+
+    button,
+    p {
+      width: 50px;
+      text-align: center;
+    }
+    button:first-child {
+      border-right: 1px solid var(--border-color);
+    }
+    p + button {
+      border-left: 1px solid var(--border-color);
+    }
+  }
+`;
+
+export const ProductTotalAmount = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+  margin-bottom: 30px;
+  h3,
+  span {
+    font-weight: 700;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  p:first-child {
+    color: var(--border-color);
+  }
+
+  p:first-child::after {
+    content: '|';
+    margin: 0 14px;
+  }
+
+  p:nth-child(2),
+  span {
+    color: var(--main-color);
+    span {
+      font-size: var(--font-xl);
+    }
+  }
+`;
+
+export const SubmitButtonWrap = styled.div`
+  display: flex;
+  gap: 14px;
+`;
